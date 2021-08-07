57255bc2c19b46ffdaa1a0e998b98368448e6cbb
Roald Nefs <info@roaldnefs.com>
2021-08-07 16:34:56 +0200

    fix: typo in migrations/2_deploy_contracts.js
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const FisherRole = artifacts.require("./FisherRole.sol");
 const DistributorRole = artifacts.require("./DistributorRole.sol");
 const RetailerRole = artifacts.require("./RetailerRole.sol");
 const ConsumerRole = artifacts.require("./ConsumerRole.sol");
-const SupplyChain = artifacts.require("./SuppyChain.sol");
+const SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(FisherRole);
