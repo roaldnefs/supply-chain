ba8f6d83677b4344b75a1efb86ead421552501a6
Roald Nefs <info@roaldnefs.com>
2021-08-11 12:00:07 +0200

    feat: implement fronend dapp
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- app/src/index.js
+++ app/src/index.js
@@ -14,7 +14,7 @@ const App = {
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = supplyChainArtifact.networks[networkId];
       this.meta = new web3.eth.Contract(
-        metaCoinArtifact.abi,
+        supplyChainArtifact.abi,
         deployedNetwork.address,
       );
 
@@ -22,37 +22,200 @@ const App = {
       const accounts = await web3.eth.getAccounts();
       this.account = accounts[0];
 
-      this.refreshBalance();
+      this.fetchEvents();
     } catch (error) {
       console.error("Could not connect to contract or chain.");
     }
   },
 
-  refreshBalance: async function() {
-    const { getBalance } = this.meta.methods;
-    const balance = await getBalance(this.account).call();
+  fetchRaw: async function() {
+    // Retrieve UPC from form
+    const upc = document.getElementById('upc').value;
 
-    const balanceElement = document.getElementsByClassName("balance")[0];
-    balanceElement.innerHTML = balance;
+    // Call fetchItemBufferRaw on the smart contract
+    const { fetchItemBufferRaw } = this.meta.methods;
+    const result = await fetchItemBufferRaw(upc).call();
+
+    // Show results on the screen
+    document.getElementById('sku').value = result.itemSKU;
+    document.getElementById('ownerID').value = result.ownerID;
+    document.getElementById('originFisherID').value = result.originFisherID;
   },
 
-  sendCoin: async function() {
-    const amount = parseInt(document.getElementById("amount").value);
-    const receiver = document.getElementById("receiver").value;
+  fetchProcessed: async function() {
+    // Retrieve UPC from form
+    const upc = document.getElementById('upc').value;
 
-    this.setStatus("Initiating transaction... (please wait)");
+    // Call fetchItemBufferProcessed on the smart contract
+    const { fetchItemBufferProcessed } = this.meta.methods;
+    const result = await fetchItemBufferProcessed(upc).call();
 
-    const { sendCoin } = this.meta.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
+    // Determine the product state
+    let state = "Unknown";
+    switch (result.itemState) {
+      case "0":
+        state = "Fished";
+        break;
+      case "1":
+        state = "For Sale";
+        break;
+      case "2":
+        state = "Sold";
+        break;
+      case "3":
+        state = "Processed";
+        break;
+      case "4":
+        state = "Packed";
+        break;
+      case "5":
+        state = "Shipped";
+        break;
+      case "6":
+        state = "Received";
+        break;
+      case "7":
+        state = "Purchased";
+        break;
+    }
 
-    this.setStatus("Transaction complete!");
-    this.refreshBalance();
+    // Show results on the screen
+    document.getElementById('productNotes').value = result.productNotes;
+    document.getElementById('productPrice').value = result.productPrice;
+    document.getElementById('distributorID').value = result.distributorID;
+    document.getElementById('retailerID').value = result.retailerID;
+    document.getElementById('consumerID').value = result.consumerID;
+    document.getElementById('itemState').value = state;
   },
 
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
+  // Fetch events from smart contract
+  fetchEvents: async function() {
+    const { allEvents } = this.meta.events;
+    let events = await allEvents();
+
+    events.on('data', function(event){
+      var element = document.getElementById('ftc-events');
+
+      if (! element.innerHTML.includes(event.transactionHash)) {
+        var entry = document.createElement('li');
+        entry.appendChild(document.createTextNode(event.event + ' - ' + event.transactionHash));
+        element.appendChild(entry);
+      }
+    });
   },
+
+  fishItem: async function () {
+    const upc = document.getElementById('upc').value;
+    const fisherID = this.account;
+    const fisherName = document.getElementById('originFisherName').value;
+    const fisherInformation = document.getElementById('originFisherInformation').value;
+    const fisherLatitude = document.getElementById('originFisherLatitude').value;
+    const fisherLongitude = document.getElementById('originFisherLongitude').value;
+    const productNotes = document.getElementById('productNotes').value;
+
+    const { fishItem } = this.meta.methods;
+    await fishItem(
+      upc,
+      fisherID,
+      fisherName,
+      fisherInformation,
+      fisherLatitude,
+      fisherLongitude,
+      productNotes
+    ).send({ from: this.account });
+
+    // Update raw product details
+    this.fetchRaw();
+    this.fetchProcessed();
+    this.fetchEvents();
+  },
+
+  sellItem: async function() {
+    const upc = document.getElementById('upc').value;
+    const price = document.getElementById('productPrice').value;
+
+    const { sellItem } = this.meta.methods;
+    await sellItem(upc, price).send({ from: this.account });
+
+    // Update raw product details
+    this.fetchRaw();
+    this.fetchProcessed();
+    this.fetchEvents();
+  },
+
+  buyItem: async function() {
+    const upc = document.getElementById('upc').value;
+    const price = document.getElementById('productPrice').value;
+
+    const { buyItem } = this.meta.methods;
+    await buyItem(upc).send({ from: this.account, value: this.web3.utils.toWei(price, "ether") });
+
+    // Update raw product details
+    this.fetchRaw();
+    this.fetchProcessed();
+    this.fetchEvents();
+  },
+
+  processItem: async function() {
+    const upc = document.getElementById('upc').value;
+
+    const { processItem } = this.meta.methods;
+    await processItem(upc).send({ from: this.account });
+
+    // Update raw product details
+    this.fetchRaw();
+    this.fetchProcessed();
+    this.fetchEvents();
+  },
+
+  packItem: async function() {
+    const upc = document.getElementById('upc').value;
+
+    const { packItem } = this.meta.methods;
+    await packItem(upc).send({ from: this.account });
+
+    // Update raw product details
+    this.fetchRaw();
+    this.fetchProcessed();
+    this.fetchEvents();
+  },
+
+  shipItem: async function() {
+    const upc = document.getElementById('upc').value;
+
+    const { shipItem } = this.meta.methods;
+    await shipItem(upc).send({ from: this.account });
+
+    // Update raw product details
+    this.fetchRaw();
+    this.fetchProcessed();
+    this.fetchEvents();
+  },
+
+  receiveItem: async function() {
+    const upc = document.getElementById('upc').value;
+
+    const { receiveItem } = this.meta.methods;
+    await receiveItem(upc).send({ from: this.account });
+
+    // Update raw product details
+    this.fetchRaw();
+    this.fetchProcessed();
+    this.fetchEvents();
+  },
+
+  purchaseItem: async function() {
+    const upc = document.getElementById('upc').value;
+    const price = document.getElementById('productPrice').value;
+
+    const { purchaseItem } = this.meta.methods;
+    await purchaseItem(upc).send({ from: this.account, value: this.web3.utils.toWei(price, "ether") });
+
+    // Update raw product details
+    this.fetchRaw();
+    this.fetchProcessed();
+    this.fetchEvents();
+  }
 };
 
 window.App = App;
