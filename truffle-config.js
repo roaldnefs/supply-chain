598f0e13e00340f490413c50cbe01eedac47cd47
Roald Nefs <info@roaldnefs.com>
2021-08-11 13:29:38 +0200

    feat: add rinkeby network cto truffle-config.js
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,13 @@
  *
  */
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 // const HDWallet = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -68,6 +70,12 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+     rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/09f6bafddaf84ae189b98d5deb05f0ba`),
+      network_id: 4,       // Rinkeby's id
+      gas: 5500000,        // Rinkeby has a lower block limit than mainnet
+    },
 
     // Useful for private networks
     // private: {
